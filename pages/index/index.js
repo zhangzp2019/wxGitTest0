daa253b6f6f5c210643dc3fc0f93ade2bdfea7c2
zhangzp2019 <501801911@qq.com>
2019-02-22 16:23:55 +0800

    第一次修改测试
    第一次修改测试，by张卓鹏

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World  test git 第一次修改',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
